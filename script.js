fc057d4411c49f86622a668f7c0c5cefbf83c24a
Amin <aminullahpuya@gmail.com>
2025-11-10 14:55:58 +0100

    updated

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const myFunc = (val) => `value: ${val}`;
 const array = [1, 2, 3];
 const newArray = array.map(myFunc);
 console.log(newArray);
+
+// it is common convention to prefix an unused parameter with an underscore _.
+// You could also leave the parameter empty like so: (, char1) but it is
+// often clearer to name the parameter for future readability.
+const addCharacters = (_prefix) => (suffix) => suffix.toUpperCase();
+console.log(addCharacters("foo")("bar")); // → "BAR"
