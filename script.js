0d57ab9850757a192f1d0785a9485671925a51e2
Amin <aminullahpuya@gmail.com>
2025-12-18 13:40:30 +0100

    definitions were added

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//
+
+/*
+Data Types in JavaScript
+Data types help the program understand the kind of data it's working with.
+
+Number: A number represents both integers and floating-point values. Examples of integers include 7, 19, and 90.
+Floating point: A floating point number is a number with a decimal point. Examples include 3.14, 0.5, and 0.0001.
+String: A string is a sequence of characters, or text, enclosed in quotes. "I like coding" and 'JavaScript is fun' are examples of strings.
+Boolean: A boolean represents one of two possible values: true or false. You can use a boolean to represent a condition, such as isLoggedIn = true.
+Undefined and Null: An undefined value is a variable that has been declared but not assigned a value. A null value is an empty value or a variable that has intentionally been assigned a value of null.
+Object: An object is a collection of key-value pairs. The key is the property name, and the value is the property value.
+
+
+*/
+
 // A function reference is a function name without the parentheses
 const myFunc = (val) => `value: ${val}`;
 const array = [1, 2, 3];
